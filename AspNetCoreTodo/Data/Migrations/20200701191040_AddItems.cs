62ed407710b6f3e5b2973ed17bbc3e90873a27bb
Alessandro <alessandro_mk@hotmail.com>
2020-07-01 18:17:49 -0300

    commit depois da alteração do sistema

---
+++ AspNetCoreTodo/Data/Migrations/20200701191040_AddItems.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspNetCoreTodo.Data.Migrations
+{
+    public partial class AddItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Dias",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Dias",
+                table: "Items");
+        }
+    }
+}
